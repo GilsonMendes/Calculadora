fb3a181123030464593fea594aeb59ddf77c65c5
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-04-09 21:33:03 -0300

    Add styles nos buttons via props.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Home from "./src/components/home";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Home from "./src/components/screen";
 import Button from "./src/components/buttons/Buntton";
+import Display from "./src/components/screen";
 function name(name: string) {
   name = "Gilson";
   console.log(name);
@@ -9,26 +11,28 @@ function name(name: string) {
 }
 
 export default function App() {
+  const [value] = useState('0');
   return (
     <View style={styles.container}>
-      <View style ={styles.buttons}>
-        <Button _name="AC" _onClick={name} />
-        <Button _name="/" _onClick={name} />
+      <Display value={value}/>
+      <View style={styles.buttons}>
+        <Button _name="AC" _onClick={name}  _triple ={true} />
+        <Button _name="/" _onClick={name} _operation={true}/>
         <Button _name="7" _onClick={name} />
         <Button _name="8" _onClick={name} />
         <Button _name="9" _onClick={name} />
-        <Button _name="*" _onClick={name} />
+        <Button _name="*" _onClick={name} _operation={true} />
         <Button _name="4" _onClick={name} />
         <Button _name="5" _onClick={name} />
         <Button _name="6" _onClick={name} />
-        <Button _name="-" _onClick={name} />
+        <Button _name="-" _onClick={name} _operation={true} />
         <Button _name="1" _onClick={name} />
         <Button _name="2" _onClick={name} />
         <Button _name="3" _onClick={name} />
-        <Button _name="+" _onClick={name} />
+        <Button _name="+" _onClick={name} _operation={true}/>
         <Button _name="0" _onClick={name} />
         <Button _name="." _onClick={name} />
-        <Button _name="=" _onClick={name} />
+        <Button _name="=" _onClick={name}  _double = {true}  />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -39,8 +43,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  buttons:{
-    flexDirection: 'row',
-
-  }
+  buttons: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
 });
--- src/components/buttons/Buntton.tsx
+++ src/components/buttons/Buntton.tsx
@@ -5,30 +5,69 @@ import {
   View,
   TouchableHighlight,
   GestureResponderEvent,
+  Button,
 } from "react-native";
 import React from "react";
 
 type getProps = {
   _name: string;
-  _onClick:(props:any)=>string;
+  _onClick: (props: any) => string;
+  _double?: boolean;
+  _triple?: boolean;
+  _operation?: boolean;
 };
 
 export default (props: getProps) => {
+  let stylesButton: object = styles.button;
+  if(props._double) stylesButton = styles.buttonDouble;
+  if(props._triple) stylesButton = styles.buttonTriple;
+  if(props._operation) stylesButton = styles.operationButton;
   return (
     <View>
       <TouchableHighlight onPress={props._onClick}>
-        <Text style={styles.button}>{props._name}</Text>
+        <Text style={stylesButton}>{props._name}</Text>
       </TouchableHighlight>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
+  button:{
     fontSize: 40,
     padding: 20,
     height: Dimensions.get("window").width / 4,
     width: Dimensions.get("window").width / 4,
+    backgroundColor: "#f0f0f0",
+    textAlign: "center",
+    borderWidth: 1,
+    borderColor: "#888",
+  },
+  operationButton: {
+    color: "#FFF",
+    backgroundColor: "#fa8231",
+    fontSize: 40,
+    padding: 20,
+    height: Dimensions.get("window").width / 4,
+    width: Dimensions.get("window").width / 4,
+    textAlign: "center",
+    borderWidth: 1,
+    borderColor: "#888",
+  },
+  buttonDouble: {
+    fontSize: 40,
+    padding: 20,
+    height: Dimensions.get("window").width / 4,
+    width: (Dimensions.get("window").width / 4) * 2,
+    backgroundColor: "#f0f0f0",
+    textAlign: "center",
+    borderWidth: 1,
+    borderColor: "#888",
+  },
+  buttonTriple: {
+    fontSize: 40,
+    padding: 20,
+    height: Dimensions.get("window").width / 4,
+    width: (Dimensions.get("window").width / 4) * 3,
     backgroundColor: "#f0f0f0",
     textAlign: "center",
     borderWidth: 1,
